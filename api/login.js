ca1106a2520baa3265e0960cab285a63b319cb2c
creciendoengrande <creciendoengrande2025@gmail.com>
2025-04-06 01:15:06 -0500

    .

--- api/login.js
+++ api/login.js
@@ -6,11 +6,11 @@ module.exports = async (req, res) => {
     res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-    // if (req.method === "OPTIONS") {
-    //     return res.status(200).end();
-    // }
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
 
-    if (req.method !== "POST") {
+    if (req.method !== "POST" || req.method !== "OPTIONS") {
         return res.status(405).json({ error: "Método no permitido" });
     }
 
