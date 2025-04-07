190cca4f2e890578ddf6e0afff82d277881e903f
creciendoengrande <creciendoengrande2025@gmail.com>
2025-04-06 23:46:18 -0500

    rules list

---
+++ api/rules.js
@@ -0,0 +1,39 @@
+const axios = require("axios");
+
+module.exports = async (req, res) => {
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+
+    if (req.method !== "POST" && req.method !== "OPTIONS") {
+        return res.status(405).json({ error: "Método no permitido" });
+    }
+
+    try {
+        const { username, password } = req.body;
+
+        const response = await axios.post(
+            "https://api.retool.com/v1/workflows/b6149907-b763-4e3a-92ad-fbd68756f911/startTrigger",
+            { username, password },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-Workflow-Api-Key": "retool_wk_651f33fe9d794e0f8f5b629f00cf162d"
+                }
+            }
+        );
+
+        res.status(200).json(response.data);
+    } catch (error) {
+        console.error("Error en proxy:", error.response?.data || error.message);
+        res.status(500).json({
+            error: "Error al comunicarse con Retool",
+            details: error.response?.data || error.message
+        });
+    }
+};
